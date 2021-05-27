947bf2a50b9b1442719695b7ddae3074ea49b44e
ジョセフ <54091798+josephcarragher@users.noreply.github.com>
2021-05-27 19:21:42 -0400

    Add files via upload

---
+++ ROCm_OpenCL_Fixer.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# This fixes the ROCm install so OpenCL actually works.
+
+# Needs sudo
+if [ `whoami` != root ]; then
+    echo Please run this script using sudo
+    exit
+fi
+
+# Find better way to do this
+cd ..
+cd ..
+amdPath=$(find /etc/OpenCL/vendors/ -type f -name 'amdocl64*.icd')
+
+# And this
+cd
+cd ..
+cd ..
+rocmPath=$(find /opt -maxdepth 1 -type d -name 'rocm-*')
+sudo echo "${rocmPath}/opencl/lib/libamdocl64.so" > ${amdPath}
+echo Done
